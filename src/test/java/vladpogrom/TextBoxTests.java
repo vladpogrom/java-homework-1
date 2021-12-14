89d3927533efc260729e51ad33a19b702aa1dcfd
vladpogrom <vladpogrom@pm.me>
2021-12-15 00:41:11 +0500

    added FirstSimpleTest and TextBoxTests

---
+++ src/test/java/vladpogrom/TextBoxTests.java
@@ -0,0 +1,38 @@
+package vladpogrom;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.conditions.Text;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class TextBoxTests {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+    }
+
+    @Test
+    void successTest() {
+        open("https://demoqa.com/text-box");
+
+        $("#userName").setValue("Alex");
+        $("#userEmail").setValue("Alex@gmail.com");
+        $("#currentAddress").setValue("Adress Alex");
+        $("#permanentAddress").setValue("Alex Permanentr");
+        $("#submit").scrollTo().click();
+
+        $("#output").shouldBe(visible);
+        $("#name").shouldHave(text("Alex"));
+        $("#email").shouldHave(text("Alex@gmail.com"));
+        $("#output #currentAddress").shouldHave(text("Adress Alex"));
+        $("#output").$("#permanentAddress").shouldHave(text("Alex Permanentr"));
+    }
+}
