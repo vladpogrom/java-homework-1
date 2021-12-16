05dac2f45018b27c322876b3458bea95e6426df1
vladpogrom <vladpogrom@pm.me>
2021-12-16 19:36:12 +0500

    Add almost all selectors except date selector and add test_file.png

--- src/test/java/vladpogrom/HomeworkTests.java
+++ src/test/java/vladpogrom/HomeworkTests.java
@@ -7,54 +7,51 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
+
 import java.io.File;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class HomeworkTests {
 
-//    @BeforeAll
-//    static void beforeAll() {
-//       Configuration.browserSize = "1920x1080";}
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+    }
 
     @Test
     void successTest() {
         open("https://demoqa.com/automation-practice-form");
-
-//        $("#userName").setValue("Alex");
-//        $("#userEmail").setValue("Alex@gmail.com");
-//        $("#currentAddress").setValue("Adress Alex");
-//        $("#permanentAddress").setValue("Alex Permanentr");
-//        $("#submit").scrollTo().click();
-//
-//        $("#output").shouldBe(visible);
-//        $("#name").shouldHave(text("Alex"));
-//        $("#email").shouldHave(text("Alex@gmail.com"));
-//        $("#output #currentAddress").shouldHave(text("Adress Alex"));
-//        $("#output").$("#permanentAddress").shouldHave(text("Alex Permanentr"));
-
-
         $("#firstName").setValue("Big");
         $("#lastName").setValue("Lebovski");
         $("#userEmail").setValue("Lebovski@gmail.com");
         $("[for=\"gender-radio-3\"]").click();
         $("#userNumber").setValue("9887987687");
-//        #datebirth
+//        $("#dateOfBirthInput");
         $("[for=\"hobbies-checkbox-1\"]").click();
         $("[for=\"hobbies-checkbox-2\"]").click();
         $("[for=\"hobbies-checkbox-3\"]").click();
-//        #picture
-//        new File("test_file.png")
-//        new DownloadFileWithHttpRequest("https://yt3.ggpht.com/a/AATXAJzizDQ--4hzM0TYKt_HBizzKgSeO3VrabeSVQ=s900-c-k-c0xffffffff-no-rj-mo");
-        $("#uploadPicture").uploadFile(new File("test_file.png"));
+        $("#uploadPicture").uploadFromClasspath("test_file.png");
         $("#currentAddress").setValue("Adress Big");
-//        #state
-//        #city
+        $("#state").scrollTo().click();
+        $(By.xpath("//div[contains(text(),'Haryana')]")).click();
+        $("#city").scrollTo().click();
+        $(By.xpath("//div[contains(text(),'Karnal')]")).click();
         $("#submit").scrollTo().click();
 
+        $("#example-modal-sizes-title-lg").shouldBe(visible).shouldHave(text("Thanks for submitting the form"));
+        $(By.xpath("//td[contains(text(),'Big Lebovski')]")).shouldHave(text("Big Lebovski"));
+        $(By.xpath("//td[contains(text(),'Lebovski@gmail.com')]")).shouldHave(text("Lebovski@gmail.com"));
+        $(By.xpath("//td[contains(text(),'Other')]")).shouldHave(text("Other"));
+        $(By.xpath("//td[contains(text(),'9887987687')]")).shouldHave(text("9887987687"));
+        //        $("#dateOfBirthInput");
+        $(By.xpath("//td[contains(text(),'Sports, Reading, Music')]")).shouldHave(text("Sports, Reading, Music"));
+        $(By.xpath("//td[contains(text(),'test_file.png')]")).shouldHave(text("test_file.png"));
+        $(By.xpath("//td[contains(text(),'Adress Big')]")).shouldHave(text("Adress Big"));
+        $(By.xpath("//td[contains(text(),'Haryana Karnal')]")).shouldHave(text("Haryana Karnal"));
+        $("#closeLargeModal").scrollIntoView(false).click();
 
     }
 }
