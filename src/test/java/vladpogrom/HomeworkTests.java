5dbdd0f029379f79607dce600c957b30789dbd3e
vladpogrom <vladpogrom@pm.me>
2021-12-17 18:45:29 +0500

    Final version, no cycle for hobbies checkboxes, but works

--- src/test/java/vladpogrom/HomeworkTests.java
+++ src/test/java/vladpogrom/HomeworkTests.java
@@ -1,48 +1,47 @@
 package vladpogrom;
 
 import com.codeborne.selenide.Configuration;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Condition.appear;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class HomeworkTests {
 
-    @BeforeAll
-    static void beforeAll() {
+    @BeforeEach
+    void openForm(){
+        clearBrowserCookies();
         Configuration.browserSize = "1920x1080";
+        open("https://demoqa.com/automation-practice-form");
     }
 
     @Test
     void fillForm() {
-        //Open form
-        open("https://demoqa.com/automation-practice-form");
-
-        //Fill easy forms
+        //Fill easy lines
         $("#firstName").setValue("Big");
         $("#lastName").setValue("Lebovski");
         $("#userEmail").setValue("Lebovski@gmail.com");
         $("[for=\"gender-radio-3\"]").click();
         $("#userNumber").setValue("9887987687");
-        $("#subjectsInput").setValue("Arts").pressEnter().setValue("English").pressEnter().pressEnter();
         $("#uploadPicture").uploadFromClasspath("test_file.png");
         $("#currentAddress").setValue("Adress Big");
-        $("#react-select-3-input").setValue("Haryana").pressEnter();
-        $("#react-select-4-input").setValue("Karnal").pressEnter();
-
-        //Run cycle for filling checkboxes todo
+        $("#react-select-3-input").setValue("Har").pressEnter();
+        $("#react-select-4-input").setValue("Kar").pressEnter();
+        $("#subjectsInput").setValue("Arts").pressEnter().setValue("English").pressEnter();
         $("[for=\"hobbies-checkbox-1\"]").click();
         $("[for=\"hobbies-checkbox-2\"]").click();
         $("[for=\"hobbies-checkbox-3\"]").click();
 
         //Fill date
-//        $("#dateOfBirthInput"); todo
+        $("#dateOfBirthInput").click();
+        $("[class*=\"year-select\"]").selectOptionByValue("1996");
+        $("[class*=\"month-select\"]").selectOptionByValue("3");
+        $("[class*=\"021\"]").click();
 
+        // Click to submit
         $("#submit").scrollTo().click();
 
         // Run check form
@@ -58,7 +57,7 @@ public class HomeworkTests {
         $(byText("Lebovski@gmail.com")).should(appear);
         $(byText("Other")).should(appear);
         $(byText("9887987687")).should(appear);
-//        $(byText("21 April,1996")).should(appear);
+        $(byText("21 April,1996")).should(appear);
         $(byText("Sports, Reading, Music")).should(appear);
         $(byText("test_file.png")).should(appear);
         $(byText("Adress Big")).should(appear);
